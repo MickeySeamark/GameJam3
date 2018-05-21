d7a0f8b0299ada37dc31be9ca99128399524983a
NerbzHub <nathan.nette@gmail.com>
2018-05-21 11:43:26 +1000

    Added basic color materials for ground, bullet and character. Added physics material for bullet. Added bullet prefab. Added Player script.

---
+++ 6 million dollar vigilante/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+
+    // Access to the shoot script
+    private GameObject shoot;
+
+    // Whether the player is out of ammo or not.
+    private bool outOfAmmo;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(outOfAmmo)
+        {
+            // Can't shoot
+            // Display text saying out of ammo.
+        }
+
+
+
+	}
+}
