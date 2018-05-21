d0cb46ec721f801e176a55f56fed574671afd0b3
NerbzHub <nathan.nette@gmail.com>
2018-05-21 12:00:34 +1000

    Object pool is working in Nathan's scene. Shoots 6 bullets.

--- 6 million dollar vigilante/Assets/Scripts/Player.cs
+++ 6 million dollar vigilante/Assets/Scripts/Player.cs
@@ -8,6 +8,9 @@ public class Player : MonoBehaviour {
     // Access to the shoot script
     private GameObject shoot;
 
+    // Shoot speed
+    public float shootSpeed = 100;
+
     // Whether the player is out of ammo or not.
     private bool outOfAmmo;
 
@@ -20,13 +23,32 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+       
+
 		if(outOfAmmo)
         {
             // Can't shoot
             // Display text saying out of ammo.
         }
 
+        // Rootin' Tootin' Shootin' 
+        if (Input.GetKeyDown(KeyCode.Mouse0) && !outOfAmmo)
+        {
+            // Get access to the bullet in the object pool.
+            GameObject copy = ObjectPool.m_SharedInstance.GetPooledObject();
+            
+            // Set the spawn point of the bullet at the players arm position.
+            copy.transform.position = transform.position + transform.forward + (transform.up * 0.6f) * 1;
 
+            // Get the snowballs rigidbody
+            Rigidbody rb = copy.GetComponent<Rigidbody>();
 
-	}
+            // Apply a 'shooting' force to the bullet.
+            rb.AddForce(transform.forward * shootSpeed, ForceMode.Impulse);
+
+            //// Tell the animator the bullet is finished being shot.
+            //m_Animator.SetBool("shooting", false);
+        }
+
+    }
 }
